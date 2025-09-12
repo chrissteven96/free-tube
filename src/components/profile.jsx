362e9bc149cbb808908e1325be3a23fd29fef658
chrissteven96 <christogram96s@gmail.com>
2025-09-11 19:04:16 -0500

    first commit

---
+++ src/components/profile.jsx
@@ -0,0 +1,5 @@
+export default function Profile() {
+    return (
+        <img src="https://i.pravatar.cc/150?u=1" alt="" />
+    );
+}
---
+++ src/renderer.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client';
+import Profile from './components/profile';
+
+const App = () => {
+
+    return (
+      <>
+        <Profile />
+      </>
+    );
+    
+};
+
+const container = document.getElementById('root');
+const root=  createRoot(container);
+root.render(<App/>);
