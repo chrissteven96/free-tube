0ca1e5a159c40bbefdafb3eaa0f9de2edce56a6d
chrissteven96 <christogram96s@gmail.com>
2025-12-07 18:20:41 -0500

    iconos

--- src/components/interface.jsx
+++ src/components/interface.jsx
@@ -100,7 +100,7 @@ function Interface() {
       <img src="./assets/christotechColor.png" alt="" className={styles.logo}/>
       </div>
 
-      <h1>Free <span className={styles.span1}>Tube</span>  <span className={styles.span2}> by Chris</span> </h1>
+      <h1>Free <span className={styles.span1}>Tube</span>  <span className={styles.span2}> by ChristoTech</span> </h1>
 
       </div>
       
